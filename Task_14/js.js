278e49c719be3c00d7554baf61c911b647711945
katy-lix <68155637+katy-lix@users.noreply.github.com>
2020-10-05 23:39:04 +0300

    added task_14
    added task_14

---
+++ Task_14/js.js
@@ -0,0 +1,87 @@
+'use strict'
+// task_1
+// let a=1;
+// let b=2;
+
+// let result = a+b;
+//  console.log(result);
+
+//  let c=1;
+// let e=2;
+//  let resulta = c-e;
+//  let resulte = a/b;
+//  console.log(resulta);
+//  console.log(resulte);
+
+//  let d=1;
+//  let f=2;
+//  let resultb = d/f;
+//  console.log(resultb);
+
+// let g=1;
+// let k=2;
+//  let resultc = g*k;
+//  console.log(resultc);
+// task_2
+// let a=20;
+// let b=15;
+
+// let aa = b;
+
+//  console.log(aa);
+// task_3
+// let num = prompt();
+// let a = 86400;
+// let seconds = num * a;
+
+// console.log (seconds);
+// alert (seconds);
+
+//  task_4
+// let calculator = {
+//     sum() {
+//       return this.a + this.b;
+//     },
+  
+//     mul() {
+//       return this.a * this.b;
+//     },
+//         del() {
+//       return this.a / this.b;
+//     },
+//            raz() {
+//       return this.a - this.b;
+//     },
+//     read() {
+//       this.a = +prompt('a?', 0);
+//       this.b = +prompt('b?', 0);
+//     }
+//   };
+  
+//   calculator.read();
+//   console.log('сумма',calculator.sum() );
+//   console.log( 'умножение',calculator.mul() );
+//   console.log('деление',calculator.del() );
+//   console.log( 'разность',calculator.raz() );
+//  task_5
+//  let button = document.querySelector('button');
+
+// button.onclick = function() {
+//   let name = prompt('What is your name?');
+//   alert('Hello ' + name + ':)');
+// }
+
+// task_8
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+
+// alert(num);
+// task_9
+let num = 10;
+++ num;
+++ num;
+-- num;
+alert(num);
